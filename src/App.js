95ca215f4f2fcd41c06f37dd22c2ebb8eb1b5e8e
Matahari Kinanti Herwin <abanyo@Matahari-Kinanti-Herwins-MacBook-Pro.local>
2020-09-15 20:03:33 -0400

    Add Basic components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+import ArticleListing from "./containers/ArticleListing";
+
+function App() {
+  return (
+    <div className="App">
+      <ArticleListing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/ArticleListing.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Component Imports
+import ArticleCard from "../components/ArticleCard";
+
+function ArticleListing() {
+  return (
+    <section>
+      <h1>Articles</h1>
+      <ArticleCard />
+    </section>
+  );
+}
+
+export default ArticleListing;
