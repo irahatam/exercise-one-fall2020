d4150ab5e15f9b08ac93160817dfcbe991fc9936
Matahari Kinanti Herwin <matahari@nyu.edu>
2020-09-28 23:37:23 -0400

    final touches

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from 'react';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch,Route } from "react-router-dom";
 import './App.css';
-
 import ArticleListing from './containers/ArticleListing';
-
 import Article from './containers/Article';
 
 function App() {
